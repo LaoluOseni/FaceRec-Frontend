abe98847616e1a66c55c3cbff047bcf886ba1eb7
Laolu Oseni <63481526+LaoluOseni@users.noreply.github.com>
2021-03-01 17:06:52 +0100

    backendddd

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -10,6 +10,7 @@ class Register extends React.Component {
         }
     }
     onNameChange = (event) => {
+        console.log(event.target.value);
         this.setState({name: event.target.value})
     }
     onEmailChange = (event) => {
