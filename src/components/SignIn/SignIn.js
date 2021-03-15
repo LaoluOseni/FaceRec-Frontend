d5944fb4f359cf83076740d06cd42b581b0ed936
Laolu Oseni <63481526+LaoluOseni@users.noreply.github.com>
2021-03-15 16:26:34 +0100

    deployment first phase

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends React.Component {
         this.setState({signInPassword: event.target.value})
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://aqueous-tundra-47495.herokuapp.com/signin', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
